f66a29210329a3c33df32f3cfb0032e7daa26516
Shams Hasan <shamshasan941@gmail.com>
2024-11-20 04:40:53 -0500

    edited date format to fix unnecessary repetition

--- angular-portfolio/src/app/mocks/mocks.ts
+++ angular-portfolio/src/app/mocks/mocks.ts
@@ -1,6 +1,6 @@
 export const Shams = {
-    skills: ["Python", "Java", "TypeScript", "JavaScript", "SQL", "Angular", ".NET", "Karma & Jasmine Testing", "JUnit Testing", "HTML/CSS", "MySQL", "AWS","Docker", "YAML", "Git", "Agile", "Figma"],
+    skills: ["Python", "Java", "TypeScript", "JavaScript", "SQL", "Angular", ".NET", "Karma & Jasmine Testing", "JUnit Testing", "HTML/CSS", "MySQL", "AWS", "Docker", "YAML", "Git", "Agile", "Figma"],
     experiences: ["Software Engineer Intern at Zebra Technologies", "Programming Tutor at Georgia State University"],
-    dates: ["May 2024 - August 2024", "October - December 2023"],
+    dates: ["May - August 2024", "October - December 2023"],
     socials: ["LinkedIn", "GitHub"]
 }
